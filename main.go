0100c8c94f8dddcd91bca506e0bc1dd0f8e6ba3c
MacBookAirM2 <uuxia@macbookairm2.com>
2023-05-24 17:42:39 +0800

    2023-05-24 17:42:39 by uuxia

--- main.go
+++ main.go
@@ -2,12 +2,11 @@ package main
 
 import (
 	"go-openai/openai"
-	"os"
 )
 
 func main() {
-	os.Setenv("OPENAI_APIKEY", "inSGSo58WBtjZDt6D6SmT3BlbkFJzYTGWVxr6OfheP8hRQ18")
-	os.Setenv("GO_APIKEY", "aiuuxia")
-	os.Setenv("SERVER_PORT", "8082")
+	//os.Setenv("OPENAI_APIKEY", "inSGSo58WBtjZDt6D6SmT3BlbkFJzYTGWVxr6OfheP8hRQ18")
+	//os.Setenv("GO_APIKEY", "aiuuxia")
+	//os.Setenv("SERVER_PORT", "8082")
 	openai.NewOpenAI().CreateOpenAiProxy()
 }
