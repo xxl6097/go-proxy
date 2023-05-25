f11c53417972a03f0140eeb1e7923620ac88a627
Your Name <you@example.com>
2023-05-25 22:17:08 +0800

    2023-05-25 22:17:08 by beck

--- main.go
+++ main.go
@@ -2,11 +2,48 @@ package main
 
 import (
 	"go-openai/openai"
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
 )
 
-func main() {
+func main1() {
 	//os.Setenv("OPENAI_APIKEY", "inSGSo58WBtjZDt6D6SmT3BlbkFJzYTGWVxr6OfheP8hRQ18")
 	//os.Setenv("GO_APIKEY", "aiuuxia")
 	//os.Setenv("SERVER_PORT", "8082")
 	openai.NewOpenAI().CreateOpenAiProxy()
+}
+
+// NewProxy takes target host and creates a reverse proxy
+// NewProxy 拿到 targetHost 后，创建一个反向代理
+func NewProxy(targetHost string) (*httputil.ReverseProxy, error) {
+	url, err := url.Parse(targetHost)
+	if err != nil {
+		return nil, err
+	}
+
+	return httputil.NewSingleHostReverseProxy(url), nil
+}
+
+// ProxyRequestHandler handles the http request using proxy
+// ProxyRequestHandler 使用 proxy 处理请求
+func ProxyRequestHandler(proxy *httputil.ReverseProxy) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		proxy.ServeHTTP(w, r)
+	}
+}
+
+func main() {
+	// initialize a reverse proxy and pass the actual backend server url here
+	// 初始化反向代理并传入真正后端服务的地址
+	proxy, err := NewProxy("https://chat.openai.com")
+	if err != nil {
+		panic(err)
+	}
+
+	// handle all requests to your server using the proxy
+	// 使用 proxy 处理所有请求到你的服务
+	http.HandleFunc("/", ProxyRequestHandler(proxy))
+	log.Fatal(http.ListenAndServe(":8888", nil))
 }
